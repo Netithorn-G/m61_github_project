6fa87dc037a436303dede51562b7855300c803bd
Netithorn-G <164364@wsk.ac.th>
2026-06-16 15:55:54 +0700

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,1 +1,15 @@
+h = int(input("Height (in centimeters):"))
+w = int(input("Weight (in kilograms):"))
 
+bmi = w/((h*h)/10000)
+
+print("Your BMI value is: ",bmi)
+
+if bmi <= 18.5:
+    print("You are underweight.")
+elif bmi < 25:
+    print("You have healthy weight.")
+elif bmi < 30:
+    print("You are overweight.")
+else:
+    print("You are obese.")
